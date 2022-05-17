3a8b3be93615cfddb86ca85bebe30dd0d19b71d4
closing-f <fql20182@upt.edu.cn>
2022-05-17 19:11:26 +0800

    init

--- test/pytorch_test.py
+++
@@ -1,8 +0,0 @@
-# import sys 
-# sys.path.append("..") 
-# from utils.arguments import parse_args
-# config =parse_args()
-# print(config.device)
-import numpy as np
-end_pos_x = np.random.randint(0, 20)*5
-print(end_pos_x) 
